ac0762b4d9150648fecf585b2b8c214985d5c204
matthewdamm <matthewjdamm@gmail.com>
2022-09-05 12:23:09 -0700

    Add files via upload

---
+++ MerkleManager_Test.java
@@ -0,0 +1,10 @@
+
+
+public class MerkleManager_Test {
+    public static void main(String[] args) {
+
+        MerkleManager managerTest = new MerkleManager();
+        managerTest.manage();
+
+    }
+}
